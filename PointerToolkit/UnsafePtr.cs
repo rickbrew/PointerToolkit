b26792a28920c6018e7a3431f5fa548373f32885
Rick Brewster <rickbrew@users.noreply.github.com>
2021-12-04 10:46:41 -0800

    Rename to PointerToolkit

---
+++ PointerToolkit/UnsafePtr.cs
@@ -0,0 +1,96 @@
+﻿using InlineIL;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace UnsafeRuntime
+{
+    public static unsafe class UnsafePtr
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref U As<T, U>(ref T* source)
+            where T : unmanaged
+            where U : unmanaged
+        {
+            IL.Emit.Ldarg_0();
+            IL.Emit.Ret();
+            throw IL.Unreachable();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref U As<T, U>(ref T** source)
+            where T : unmanaged
+            where U : unmanaged
+        {
+            IL.Emit.Ldarg_0();
+            IL.Emit.Ret();
+            throw IL.Unreachable();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref U As<T, U>(ref T*** source)
+            where T : unmanaged
+            where U : unmanaged
+        {
+            IL.Emit.Ldarg_0();
+            IL.Emit.Ret();
+            throw IL.Unreachable();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref T As<T>(ref void* source)
+            where T : unmanaged
+        {
+            IL.Emit.Ldarg_0();
+            IL.Emit.Ret();
+            throw IL.Unreachable();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref T As<T>(ref void** source)
+            where T : unmanaged
+        {
+            IL.Emit.Ldarg_0();
+            IL.Emit.Ret();
+            throw IL.Unreachable();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref T As<T>(ref void*** source)
+            where T : unmanaged
+        {
+            IL.Emit.Ldarg_0();
+            IL.Emit.Ret();
+            throw IL.Unreachable();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref U* AsPointer<T, U>(ref T source)
+            where T : unmanaged
+            where U : unmanaged
+        {
+            IL.Emit.Ldarg_0();
+            IL.Emit.Ret();
+            throw IL.Unreachable();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref U** AsPointer2<T, U>(ref T source)
+            where T : unmanaged
+            where U : unmanaged
+        {
+            IL.Emit.Ldarg_0();
+            IL.Emit.Ret();
+            throw IL.Unreachable();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref U*** AsPointer3<T, U>(ref T source)
+            where T : unmanaged
+            where U : unmanaged
+        {
+            IL.Emit.Ldarg_0();
+            IL.Emit.Ret();
+            throw IL.Unreachable();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PointerToolkit
+# UnsafeRuntime
 Provides structs that wrap pointers, and Unsafe methods for converting to and from `ref` pointers of any type.
 
 Even using the built-in `Unsafe` class, you cannot reinterpret raw pointers for use in calls to methods such as `Interlocked.Exchange()` or `Volatile.Read()`. This package allows you to do stuff like that.
