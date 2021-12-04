117954a6e4d5359ebfd7b2d3abdddd07613565ff
Rick Brewster <rickbrew@users.noreply.github.com>
2021-12-04 11:45:58 -0800

    Update UnsafePtr.cs

--- PointerToolkit/UnsafePtr.cs
+++ PointerToolkit/UnsafePtr.cs
@@ -7,6 +7,33 @@ namespace PointerToolkit;
 public static unsafe class UnsafePtr
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref T As<T>(ref void* source)
+        where T : unmanaged
+    {
+        IL.Emit.Ldarg_0();
+        IL.Emit.Ret();
+        throw IL.Unreachable();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref T As<T>(ref void** source)
+        where T : unmanaged
+    {
+        IL.Emit.Ldarg_0();
+        IL.Emit.Ret();
+        throw IL.Unreachable();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ref T As<T>(ref void*** source)
+        where T : unmanaged
+    {
+        IL.Emit.Ldarg_0();
+        IL.Emit.Ret();
+        throw IL.Unreachable();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ref U As<T, U>(ref T* source)
         where T : unmanaged
         where U : unmanaged
@@ -30,33 +57,6 @@ public static unsafe class UnsafePtr
     public static ref U As<T, U>(ref T*** source)
         where T : unmanaged
         where U : unmanaged
-    {
-        IL.Emit.Ldarg_0();
-        IL.Emit.Ret();
-        throw IL.Unreachable();
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ref T As<T>(ref void* source)
-        where T : unmanaged
-    {
-        IL.Emit.Ldarg_0();
-        IL.Emit.Ret();
-        throw IL.Unreachable();
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ref T As<T>(ref void** source)
-        where T : unmanaged
-    {
-        IL.Emit.Ldarg_0();
-        IL.Emit.Ret();
-        throw IL.Unreachable();
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ref T As<T>(ref void*** source)
-        where T : unmanaged
     {
         IL.Emit.Ldarg_0();
         IL.Emit.Ret();
