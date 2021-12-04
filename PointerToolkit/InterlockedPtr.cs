34e727fe659f0805ad14d112364f4cbe1fee1bbf
Rick Brewster <rickbrew@users.noreply.github.com>
2021-12-04 11:15:34 -0800

    Add `InterlockedPtr` and `VolatilePtr` classes

---
+++ PointerToolkit/InterlockedPtr.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace PointerToolkit
+{
+    public static unsafe class InterlockedPtr
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void* CompareExchange(ref void* location1, void* value, void* comparand)
+        {
+            return (void*)Interlocked.CompareExchange(ref UnsafePtr.As<IntPtr>(ref location1), (IntPtr)value, (IntPtr)comparand);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void** CompareExchange(ref void** location1, void** value, void** comparand)
+        {
+            return (void**)Interlocked.CompareExchange(ref UnsafePtr.As<IntPtr>(ref location1), (IntPtr)value, (IntPtr)comparand);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void*** CompareExchange(ref void*** location1, void*** value, void*** comparand)
+        {
+            return (void***)Interlocked.CompareExchange(ref UnsafePtr.As<IntPtr>(ref location1), (IntPtr)value, (IntPtr)comparand);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T* CompareExchange<T>(ref T* location1, T* value, T* comparand)
+            where T : unmanaged
+        {
+            return (T*)Interlocked.CompareExchange(ref UnsafePtr.As<T, IntPtr>(ref location1), (IntPtr)value, (IntPtr)comparand);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T** CompareExchange<T>(ref T** location1, T** value, T** comparand)
+            where T : unmanaged
+        {
+            return (T**)Interlocked.CompareExchange(ref UnsafePtr.As<T, IntPtr>(ref location1), (IntPtr)value, (IntPtr)comparand);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T*** CompareExchange<T>(ref T*** location1, T*** value, T*** comparand)
+            where T : unmanaged
+        {
+            return (T***)Interlocked.CompareExchange(ref UnsafePtr.As<T, IntPtr>(ref location1), (IntPtr)value, (IntPtr)comparand);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void* Exchange(ref void* location1, void* value)
+        {
+            return (void*)Interlocked.Exchange(ref UnsafePtr.As<IntPtr>(ref location1), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void** Exchange(ref void** location1, void** value)
+        {
+            return (void**)Interlocked.Exchange(ref UnsafePtr.As<IntPtr>(ref location1), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void*** Exchange(ref void*** location1, void*** value)
+        {
+            return (void***)Interlocked.Exchange(ref UnsafePtr.As<IntPtr>(ref location1), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T* Exchange<T>(ref T* location1, T* value)
+            where T : unmanaged
+        {
+            return (T*)Interlocked.Exchange(ref UnsafePtr.As<T, IntPtr>(ref location1), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T** Exchange<T>(ref T** location1, T** value)
+            where T : unmanaged
+        {
+            return (T**)Interlocked.Exchange(ref UnsafePtr.As<T, IntPtr>(ref location1), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T*** Exchange<T>(ref T*** location1, T*** value)
+            where T : unmanaged
+        {
+            return (T***)Interlocked.Exchange(ref UnsafePtr.As<T, IntPtr>(ref location1), (IntPtr)value);
+        }
+    }
+}
---
+++ PointerToolkit/VolatilePtr.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace PointerToolkit
+{
+    public static unsafe class VolatilePtr
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void* Read(ref void* location)
+        {
+            return (void*)Volatile.Read(ref UnsafePtr.As<IntPtr>(ref location));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void** Read(ref void** location)
+        {
+            return (void**)Volatile.Read(ref UnsafePtr.As<IntPtr>(ref location));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void*** Read(ref void*** location)
+        {
+            return (void***)Volatile.Read(ref UnsafePtr.As<IntPtr>(ref location));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T* Read<T>(ref T* location)
+            where T : unmanaged
+        {
+            return (T*)Volatile.Read(ref UnsafePtr.As<T, IntPtr>(ref location));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T** Read<T>(ref T** location)
+        where T : unmanaged
+        {
+            return (T**)Volatile.Read(ref UnsafePtr.As<T, IntPtr>(ref location));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T*** Read<T>(ref T*** location)
+            where T : unmanaged
+        {
+            return (T***)Volatile.Read(ref UnsafePtr.As<T, IntPtr>(ref location));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write(ref void* location, void* value)
+        {
+            Volatile.Write(ref UnsafePtr.As<IntPtr>(ref location), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write(ref void** location, void** value)
+        {
+            Volatile.Write(ref UnsafePtr.As<IntPtr>(ref location), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write(ref void*** location, void*** value)
+        {
+            Volatile.Write(ref UnsafePtr.As<IntPtr>(ref location), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write<T>(ref T* location, T* value)
+            where T : unmanaged
+        {
+            Volatile.Write(ref UnsafePtr.As<T, IntPtr>(ref location), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write<T>(ref T** location, T** value)
+        where T : unmanaged
+        {
+            Volatile.Write(ref UnsafePtr.As<T, IntPtr>(ref location), (IntPtr)value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write<T>(ref T*** location, T*** value)
+            where T : unmanaged
+        {
+            Volatile.Write(ref UnsafePtr.As<T, IntPtr>(ref location), (IntPtr)value);
+        }
+    }
+}
